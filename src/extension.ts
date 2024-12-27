5c3138602f7d4028a1a906c61cd4fbb88e5c7dc9
eatyourpeas <simon@eatyourpeas.co.uk>
2024-12-27 20:10:29 +0000

    add error handling

--- src/extension.ts
+++ src/extension.ts
@@ -4,105 +4,102 @@ import { fetchGitHubUsername, getGitHubToken } from './credentials';
 import { getRepoAndBranch } from './credentials';
 import { saveResultToGist } from './gist';
 
-// Load environment variables from .env file in src/env directory
-// dotenv.config({ path: path.join(__dirname, '..', 'envs', '.env') });
-
-
-let startTime: Date | undefined;
 let endTime: Date | undefined;
 let githubUsername: string | undefined;
 let token: string | undefined;
 
 export async function activate(context: vscode.ExtensionContext) {
-    const { repoName, branchName } = await getRepoAndBranch();
-    token = await getGitHubToken(context=context);
-    githubUsername = await fetchGitHubUsername(token=token);
-    if (!token) {
-        vscode.window.showErrorMessage('GitHub token is not set. Please set the GITHUB_TOKEN_TRACK_YOUR_PEAS environment variable.');
-        return;
-    }
-    vscode.window.showInformationMessage(`Congratulations ${githubUsername}, your extension "trackyourpeas" is now active! Repo: ${repoName}, Branch: ${branchName}`);
-
-    let tracking = false;
-    let paused = false;
-    let elapsedSeconds = 0;
-    let interval: NodeJS.Timeout | undefined;
-
-    const startStop = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-    startStop.tooltip = 'Start tracking your peas...';
-    startStop.command = 'trackyourpeas.startStopTracking';
-    startStop.text = '$(notebook-execute) Start Tracking';
-    startStop.show();
-
-    const pause = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-    pause.tooltip = 'Pause tracking your peas...';
-    pause.command = 'trackyourpeas.pauseTracking';
-    pause.text = '$(debug-pause) Pause';
-    pause.hide();
-
-    const startStopCommand = vscode.commands.registerCommand('trackyourpeas.startStopTracking', async () => {
-        if (tracking) {
-            // Stop tracking
-            tracking = false;
-            paused = false;
-            endTime = new Date();
-            const result = formatTime(elapsedSeconds);
-            elapsedSeconds = 0;
-            startStop.text = '$(notebook-execute) Start Tracking';
-            startStop.tooltip = 'Start tracking your peas...';
-            pause.hide();
-            if (interval) {
-                clearInterval(interval);
-            }
-            
-            
-            if (!repoName || !branchName) {
-                vscode.window.showErrorMessage('Failed to get repository and branch name.');
-                return;
-            } 
-            if (githubUsername){
-                await saveResultToGist(token, githubUsername, endTime, result, repoName, branchName);
-            }
-        } else {
-            // Start tracking
-            tracking = true;
-            paused = false;
-            elapsedSeconds = 0;
-            startTime = new Date();
-            startStop.text = '$(primitive-square) Stop Tracking';
-            startStop.tooltip = 'Stop tracking your peas...';
-            pause.show();
-            interval = setInterval(() => {
-                if (!paused) {
-                    elapsedSeconds++;
-                    const time = formatTime(elapsedSeconds);
-                    startStop.text = `$(primitive-square) Tracking... ${time}`;
-                }
-            }, 1000);
+    try {
+        const { repoName, branchName } = await getRepoAndBranch();
+        token = await getGitHubToken(context=context);
+        githubUsername = await fetchGitHubUsername(token=token);
+        if (!token) {
+            vscode.window.showErrorMessage('GitHub token is not set. Please set the GITHUB_TOKEN_TRACK_YOUR_PEAS environment variable.');
+            return;
         }
-    });
+        vscode.window.showInformationMessage(`Congratulations ${githubUsername}, your extension "trackyourpeas" is now active! Repo: ${repoName}, Branch: ${branchName}`);
 
-    const pauseCommand = vscode.commands.registerCommand('trackyourpeas.pauseTracking', () => {
-        if (tracking) {
-            if (paused) {
-                // Resume tracking
+        let tracking = false;
+        let paused = false;
+        let elapsedSeconds = 0;
+        let interval: NodeJS.Timeout | undefined;
+
+        const startStop = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        startStop.tooltip = 'Start tracking your peas...';
+        startStop.command = 'trackyourpeas.startStopTracking';
+        startStop.text = '$(notebook-execute) Start Tracking';
+        startStop.show();
+
+        const pause = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        pause.tooltip = 'Pause tracking your peas...';
+        pause.command = 'trackyourpeas.pauseTracking';
+        pause.text = '$(debug-pause) Pause';
+        pause.hide();
+
+        const startStopCommand = vscode.commands.registerCommand('trackyourpeas.startStopTracking', async () => {
+            if (tracking) {
+                // Stop tracking
+                tracking = false;
                 paused = false;
-                pause.text = '$(debug-pause) Pause';
-                pause.tooltip = 'Pause tracking your peas...';
+                endTime = new Date();
+                const result = formatTime(elapsedSeconds);
+                elapsedSeconds = 0;
+                startStop.text = '$(notebook-execute) Start Tracking';
+                startStop.tooltip = 'Start tracking your peas...';
+                pause.hide();
+                if (interval) {
+                    clearInterval(interval);
+                }
+                
+                
+                if (!repoName || !branchName) {
+                    vscode.window.showErrorMessage('Failed to get repository and branch name.');
+                    return;
+                } 
+                if (githubUsername){
+                    await saveResultToGist(token, githubUsername, endTime, result, repoName, branchName);
+                }
             } else {
-                // Pause tracking
-                paused = true;
-                pause.text = '$(debug-continue) Resume';
-                pause.tooltip = 'Resume tracking your peas...';
+                // Start tracking
+                tracking = true;
+                paused = false;
+                elapsedSeconds = 0;
+                startStop.text = '$(primitive-square) Stop Tracking';
+                startStop.tooltip = 'Stop tracking your peas...';
+                pause.show();
+                interval = setInterval(() => {
+                    if (!paused) {
+                        elapsedSeconds++;
+                        const time = formatTime(elapsedSeconds);
+                        startStop.text = `$(primitive-square) Tracking... ${time}`;
+                    }
+                }, 1000);
             }
-        }
-    });
+        });
 
-    context.subscriptions.push(startStopCommand);
-    context.subscriptions.push(pauseCommand);
-    context.subscriptions.push(startStop);
-    context.subscriptions.push(pause);
+        const pauseCommand = vscode.commands.registerCommand('trackyourpeas.pauseTracking', () => {
+            if (tracking) {
+                if (paused) {
+                    // Resume tracking
+                    paused = false;
+                    pause.text = '$(debug-pause) Pause';
+                    pause.tooltip = 'Pause tracking your peas...';
+                } else {
+                    // Pause tracking
+                    paused = true;
+                    pause.text = '$(debug-continue) Resume';
+                    pause.tooltip = 'Resume tracking your peas...';
+                }
+            }
+        });
 
+        context.subscriptions.push(startStopCommand);
+        context.subscriptions.push(pauseCommand);
+        context.subscriptions.push(startStop);
+        context.subscriptions.push(pause);
+    } catch (error) {
+        vscode.window.showErrorMessage(`Error: ${error.message}`);
+    }
     
 }
 
